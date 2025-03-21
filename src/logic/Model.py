9bf34cbee5f4ee1688da8b09d64904b416dd6c5e
junwen liu <836576596@qq.com>
2025-03-21 09:57:47 +0800

    implement is_winner

--- src/logic/Model.py
+++ src/logic/Model.py
@@ -1,6 +1,8 @@
 from src.rendering.Board import Board
 
 
+PIECES_TO_WIN_IN_LINE = 5
+
 class Model:
     def __init__(self, size: int = 15):
         self._board = Board(size)
@@ -74,7 +76,29 @@ class Model:
         Otherwise, return 1 for the winning of black, or 2 for the winning of white.
         Then change the attribute 'game_over' to True.
         """
-        raise NotImplementedError
+        curr_piece = self.piece(x, y)
+        if curr_piece == 0:
+            return 0
+
+        directions = [[1, 0], [1, 1], [0, 1], [-1, 1]]
+
+        for dx, dy in directions:
+            # 'i' indicates that curr_piece is the (i -1)th piece in the line of the direction
+            for i in range(PIECES_TO_WIN_IN_LINE):
+                x_start = x - i * dx
+                y_start = y - i * dy
+                x_end = x_start + (PIECES_TO_WIN_IN_LINE - 1) * dx
+                y_end = y_start + (PIECES_TO_WIN_IN_LINE - 1) * dy
+                # check if out of bounds
+                if not (0 <= x_start < self.size() and 0 <= x_end < self.size() and
+                        0 <= y_start < self.size() and 0 <= y_end < self.size()):
+                    continue
+                # check if five adjacent pieces in line
+                if all(self.piece(x_start + j * dx, y_start + j * dy) == curr_piece
+                    for j in range(PIECES_TO_WIN_IN_LINE)):
+                    return curr_piece
+
+        raise 0
 
     def __str__(self):
         """Generate human-readable string representation of the board.
